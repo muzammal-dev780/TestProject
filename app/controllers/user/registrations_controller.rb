6bf889e39cea136045ca224601749a641fc378b2
muzammaldev-780 <muzammal.murtaza@devsinc.com>
2020-09-23 17:14:40 +0500

    user registration issues resolved

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -12,17 +12,29 @@ class User::RegistrationsController < Devise::RegistrationsController
       send_welcome_mail(@user)
       redirect_to new_user_session_path
     else
-      render 'new'
+      render :new
+    end
+  end
+
+  def update
+    if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
+    end
+    if current_user.update(user_params)
+      redirect_to root_url
+    else
+      render :edit
     end
   end
 
   private
 
   def send_welcome_mail(user)
-    UserMailer.welcome_email(user).deliver_now
+    UserMailer.welcome_email(user).deliver_later
   end
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :role, :avatar)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :role, :avatar, :avatar_cache)
   end
 end
