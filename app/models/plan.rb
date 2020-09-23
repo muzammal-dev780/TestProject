11bae3fde93553d8ec4f9b807e34a3ac4c705713
muzammaldev-780 <muzammal.murtaza@devsinc.com>
2020-09-23 12:41:27 +0500

    some minor changes of links in header file

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -3,7 +3,7 @@
 # Plan model
 class Plan < ApplicationRecord
   has_many :features, dependent: :destroy
-  has_many :subscriptions
+  has_many :subscriptions, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
   validates :fee, presence: true
